0d44aae38bde48eb3198ce679c025bdac3dfefbb
sid <sid@sid-PC>
2015-05-11 09:00:15 +0800

    增加格式判断

--- code/utils/src/main/java/utils/utils/DataTypeUtil.java
+++ code/utils/src/main/java/utils/utils/DataTypeUtil.java
@@ -17,6 +17,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -1700,6 +1701,32 @@ public class DataTypeUtil {
 		}
 		
 		return true;
+	}
+
+
+	/**
+	 * 验证指定的一起格式
+	 * isValidDate:
+	 *
+	 * @author sid
+	 * @param str
+	 * @param formate 例子：yyyy/MM/dd
+	 * @return
+	 */
+	public static boolean isValidDate(String str,String formate) {
+		boolean convertSuccess = true;// 指定日期格式为四位年/两位月份/两位日期，注意yyyy/MM/dd区分大小写
+		SimpleDateFormat format = new SimpleDateFormat(formate);
+		try {
+			// 设置lenient为false.
+			// 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+			format.setLenient(false);
+			format.parse(str);
+		} catch (ParseException e) {
+			// e.printStackTrace();
+			// 如果throw java.text.ParseException或者NullPointerException，就说明格式不对
+			convertSuccess = false;
+		}
+		return convertSuccess;
 	}
 	
 	public static void main(String[] args) {
